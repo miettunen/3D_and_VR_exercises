adb2ae5d6963574f6ba52978eae1f9d33cee2833
Teemu Miettunen <teemu.miettunen@tuni.fi>
2021-12-10 13:57:13 +0200

    Added sorting polygons based on depth

--- LW_3/LW3_Files/sort_polygons.m
+++ LW_3/LW3_Files/sort_polygons.m
@@ -1,3 +1,11 @@
 function [connectivity_sorted] = sort_polygons(vertices, connectivity)
-
+    [m,n] = size(connectivity);
+    depth = zeros([1,n]);
+    for i = 1:n
+        % Find points from each triangle and calculate depth (average of z)
+        points = vertices(:,connectivity(:,i));
+        depth(i) = sum(points(3,:));
+    end
+    [~, index] = sort(depth, 'descend');
+    connectivity_sorted = connectivity(:,index);
 end
